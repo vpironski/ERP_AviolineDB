6e572b02a382fcd8945c04f297230212bfd2acf0
Vihren Pironski <91182968+vpironski@users.noreply.github.com>
2024-10-10 13:15:56 +0300

    Update new_employee.sql
    Fixing new employee

--- transactions/new_employee.sql
+++ transactions/new_employee.sql
@@ -1,4 +1,8 @@
+START TRANSACTION;
+
 INSERT INTO `erp_avioline`.`employees` 
 (`name`, `adress`, `age`, `gender`, `info`, `classification_idclassification`) 
 VALUES 
 ('John Doe', '303 Birch St', 29, 'Male', 'Junior Pilot', 1);
+
+COMMIT;
