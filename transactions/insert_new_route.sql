1db0cd8b1daa26eaf2bab984d79a220c902d2770
Vihren Pironski <91182968+vpironski@users.noreply.github.com>
2024-10-10 13:15:01 +0300

    Update insert_new_route.sql
    Fixing insert_new_route

--- transactions/insert_new_route.sql
+++ transactions/insert_new_route.sql
@@ -1,3 +1,13 @@
-UPDATE `erp_avioline`.`planes` 
-SET `fuelCapacity` = 25000 
-WHERE `idplanes` = 1; -- Assuming idplanes 1 corresponds to a specific plane
+START TRANSACTION;
+
+INSERT INTO `erp_avioline`.`routes` 
+(`startPoint`, `finishPoint`, `starHour`, `finishHour`, `lenght`) 
+VALUES 
+('New York', 'Chicago', '2024-10-15 10:00:00', '2024-10-15 12:00:00', '790 km'),
+('Los Angeles', 'Miami', '2024-10-16 14:00:00', '2024-10-16 18:00:00', '3500 km'),
+('Dallas', 'San Francisco', '2024-10-17 09:00:00', '2024-10-17 12:00:00', '2500 km'),
+('Seattle', 'Denver', '2024-10-18 08:00:00', '2024-10-18 11:00:00', '1600 km'),
+('Houston', 'Atlanta', '2024-10-19 15:00:00', '2024-10-19 18:00:00', '1200 km');
+
+
+COMMIT;
