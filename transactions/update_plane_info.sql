f35425a255260b2ed5dab00cd49306f023156442
vpironski <vpironski@gmail.com>
2026-03-15 22:50:47 +0200

    commit post migration

--- transactions/update_plane_info.sql
+++ transactions/update_plane_info.sql
@@ -1,11 +1,8 @@
 START TRANSACTION;
 
 UPDATE `erp_avioline`.`planes` 
-SET 
-    `manufacturer` = 'Boeing',
-    `maxSpeed` = 900, 
-    `fuelCapacity` = 200000 
-WHERE 
-    `idplanes` = 1;
+SET `fuelCapacity` = 25000 
+WHERE `idplanes` = 1; 
+-- Assuming idplanes 1 corresponds to a specific plane
 
 COMMIT;
